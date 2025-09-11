d2b40314e185a171bc52d2366d6a15373dbb471d
Adrian Ratajczak <106772824+Adrianowsski@users.noreply.github.com>
2025-09-11 18:14:11 +0200

    Add SmokeTests for basic truth assertion

--- tests/BudgetWise.API.Tests/Unit/SmokeTests.cs
+++ tests/BudgetWise.API.Tests/Unit/SmokeTests.cs
@@ -1,1 +1,14 @@
+using Xunit;
+using FluentAssertions;
+
+namespace BudgetWise.API.Tests.Unit;
+
+public class SmokeTests
+{
+    [Fact]
+    public void Truth_should_hold()
+    {
+        true.Should().BeTrue();
+    }
+}
 
